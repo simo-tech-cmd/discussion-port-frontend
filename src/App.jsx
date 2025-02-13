ea4a8d8ec1a26b8edfe2b395ce2c5b58d67bf201
simo-tech-cmd <simonwaithakawangui@gmail.com>
2025-02-13 15:18:36 +0300

    thats my part

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     <p>welcome to our discussion page</p>
 
         <h2>Im simon by the way</h2>
+        <p>atleast am flowing well</p>
 
         <h3>This page is still under construction</h3>
     </>
